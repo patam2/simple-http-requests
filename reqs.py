8629d221213f65157cf4c8aeadea1c4054995938
0 <88111864+patam2@users.noreply.github.com>
2022-01-30 19:57:07 +0200

    post request compability
    can only do application json + pep8

--- reqs.py
+++ reqs.py
@@ -1,83 +1,86 @@
 import ssl
 import socket
-import re
 import formatting
-import time
+import json as jsonmodule
 
-BLOCKSIZE = 1024 ** 2
-#ssl_context = ssl.create_default_context()
-
-##sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+BLOCKSIZE = 1024**2
 
 ####TODO:
-#* Connection lost -> refreshing
+# * Connection lost -> refreshing
+
 
 class Response:
-    def __init__(self, body, headers) -> None:
+    def __init__(self, body, headers, status_code) -> None:
         self.headers = headers
         self.body = body
+        self.status_code = int(status_code)
+
     @property
     def cookies(self):
-        if 'set-cookie' in self.headers:
-            return self.headers['set-cookie']
+        if "set-cookie" in self.headers:
+            return self.headers["set-cookie"]
         else:
             return None
 
-class Session():
-    def __init__(self, url:str) -> None:
+    @property
+    def json(self):
+        return jsonmodule.loads(self.body)
+
+
+class Session:
+    def __init__(self, url: str) -> None:
         self.host, self.port = formatting.parse_url(url)
         self.socket_session = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket_session.connect((self.host, self.port))
         if self.port == 443:
             self.socket_session = ssl.wrap_socket(
                 self.socket_session,
-                #server_hostname=self.host, 
-                server_side=False, 
+                server_side=False,
                 do_handshake_on_connect=False,
-                ssl_version=ssl.PROTOCOL_TLSv1_2
+                ssl_version=ssl.PROTOCOL_TLSv1_2,
             )
 
-    def get(self, page, headers=None):
-        block_size = 1024 ** 2
+    def request(self, method, page, headers={}, json=None):
+        method, body = method.upper(), ""
+        message = f"{method} {page} HTTP/1.1\r\nHost: {self.host}\r\n"
 
-        self.socket_session.send(
-            f'GET {page} HTTP/1.1\r\n'
-            f'Host: {self.host}'
-            '\r\n\r\n'
-            .encode()
-        )
+        if method == "POST":
+            headers["content-length"] = len(str(json))
+            headers["content-type"] = "application/json"
+            body = str(json)
 
-        data, received = self.socket_session \
-            .recv(block_size) \
-            .split(b"\r\n\r\n", 1)
+        if isinstance(headers, dict) and len(headers) != 0:
+            for i, k in headers.items():
+                message += f"{i}: {k}\r\n"
+    
+        if body != '':
+            message += "\r\n" + body + "\r\n\r\n"
+        else:
+            message += '\r\n'
+        self.socket_session.send(message.encode())
 
-        headers = dict(
-            sorted(
-                formatting.parse_request_data(data) \
-                .items()
-            )
-        ) #todo better foramting etc
-        #301 checking
+        data, received = self.socket_session.recv(BLOCKSIZE).split(b"\r\n\r\n", 1)
+        headers, status_code = formatting.parse_request_data(data)
+        # 301 checking
 
-
-        if b'301 Moved Permanently' in data or b"308 Permanent Redirect" in data:
-            #self.socket_session.shutdown(2)
-            redirect = headers['location']
-
+        if b"301 Moved Permanently" in data or b"308 Permanent Redirect" in data:
+            redirect = headers["location"]
             parsed = formatting.parse_url(redirect)
             self.host = parsed[0]
-            return self.get(parsed[1])
-        
-        if 'headers' not in headers:
+            return self.request(method, parsed[1], headers)
+        if "content-length" not in headers:
             while True:
-                received += bytes(self.socket_session.recv(block_size))
-                if received.endswith(b'\r\n\r\n'):
+                received += bytes(self.socket_session.recv(BLOCKSIZE))
+                if received.endswith(b"\r\n\r\n"):
                     break
-
-        elif int(headers['content-length']) > len(received):
-            content_length = int(headers['content-length'])
+        elif int(headers["content-length"]) > len(received):
+            content_length = int(headers["content-length"])
             while len(received) < content_length:
                 received += bytes(self.socket_session.recv(content_length))
-        
-        
-        return Response(received.decode(), headers)
+        return Response(received.decode(), headers, status_code)
+
+    def get(self, page, headers={}):
+        return self.request("get", page, headers)
+
+    def post(self, page, headers={}, json=None):
+        return self.request("post", page, headers, json)
